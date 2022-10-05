8540cc3f9b164938b6f4ce71c1eaf5ae416ff687
Marcus Pousette <marcus.pousette@quantleaf.com>
2022-10-05 11:09:44 +0200

    add exports

--- packages/ipfs/ipfs-log/src/index.ts
+++ packages/ipfs/ipfs-log/src/index.ts
@@ -3,3 +3,6 @@ export * from './log-sorting';
 export * from './log-errors';
 export * from './log-io';
 export * from './default-access-controller';
+export * from './entry';
+export * from './utils';
+export * from './lamport-clock';
