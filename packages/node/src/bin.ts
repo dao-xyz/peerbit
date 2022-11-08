c8321a840502bd758fa4573a58d260c36badf07c
Marcus Pousette <marcus.pousette@quantleaf.com>
2022-11-08 10:41:49 +0000

    move node module to https://github.com/dao-xyz/peerbit-node

--- packages/node/src/bin.ts
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-//@ts-ignore
-import { cli } from './cli.mjs'; // we have to do this because the shebang does not convert .js to .mjs
-cli()
--- packages/node/src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './cli.js';
-export * from './node.js'
-export * from './ipfs.js';
