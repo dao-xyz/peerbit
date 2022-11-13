da797e1766e2e307faf76d54ea3d91e41342efbc
Marcus Pousette <marcus.pousette@quantleaf.com>
2022-11-13 21:35:55 +0100

    fmt

--- jest.config.integration.ts
+++ jest.config.integration.ts
@@ -1,30 +1,30 @@
 import type { JestConfigWithTsJest } from "ts-jest";
 
 const jestConfig: JestConfigWithTsJest = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  roots: ["./packages/"],
-  transform: {
-    "^.+\\.tsx?$": [
-      "ts-jest",
-      {
-        useESM: true,
-      },
-    ],
-  },
-  fakeTimers: {},
-  extensionsToTreatAsEsm: [".ts"],
-  moduleNameMapper: {
-    "^(\\.{1,2}/.*)\\.js$": "$1",
-  },
-  transformIgnorePatterns: [],
+    preset: "ts-jest",
+    testEnvironment: "node",
+    roots: ["./packages/"],
+    transform: {
+        "^.+\\.tsx?$": [
+            "ts-jest",
+            {
+                useESM: true,
+            },
+        ],
+    },
+    fakeTimers: {},
+    extensionsToTreatAsEsm: [".ts"],
+    moduleNameMapper: {
+        "^(\\.{1,2}/.*)\\.js$": "$1",
+    },
+    transformIgnorePatterns: [],
 
-  /*  useESM: true, */
-  testRegex: "/__tests__/[A-Za-z0-9-/]+(\\.integration)?\\.(test|spec)\\.ts$",
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  testTimeout: 6600000,
-  setupFilesAfterEnv: ["jest-extended/all"],
-  /*   coverageReporters: ["lcov"]
-   */
+    /*  useESM: true, */
+    testRegex: "/__tests__/[A-Za-z0-9-/]+(\\.integration)?\\.(test|spec)\\.ts$",
+    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+    testTimeout: 6600000,
+    setupFilesAfterEnv: ["jest-extended/all"],
+    /*   coverageReporters: ["lcov"]
+     */
 };
 export default jestConfig;
