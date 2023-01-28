3bec40f5ccf72652f7ad987550e28baded832a38
Marcus Pousette <marcus.pousette@quantleaf.com>
2023-01-28 14:27:30 +0100

    ignore frontend build

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,29 +1,29 @@
 module.exports = {
-    "root": true,
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "ignorePatterns": ["**/*.test.ts", "/**/lib/", "/**/frontend/build", "/**/test-utils/"],
-    "rules": {
-        "@typescript-eslint/no-explicit-any": "off",
-        "@typescript-eslint/prefer-as-const": "off",
-        "@typescript-eslint/no-empty-interface": "off",
-        "no-useless-escape": "off",
-        "no-return-await": "error",
-        //"require-await": "error" TODO
-    },
+	"root": true,
+	"env": {
+		"browser": true,
+		"es2021": true,
+		"node": true
+	},
+	"extends": [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended"
+	],
+	"parser": "@typescript-eslint/parser",
+	"parserOptions": {
+		"ecmaVersion": "latest",
+		"sourceType": "module"
+	},
+	"plugins": [
+		"@typescript-eslint"
+	],
+	"ignorePatterns": ["**/*.test.ts", "/**/lib/", "/**/frontend/dist", "/**/test-utils/"],
+	"rules": {
+		"@typescript-eslint/no-explicit-any": "off",
+		"@typescript-eslint/prefer-as-const": "off",
+		"@typescript-eslint/no-empty-interface": "off",
+		"no-useless-escape": "off",
+		"no-return-await": "error",
+		//"require-await": "error" TODO
+	},
 }
