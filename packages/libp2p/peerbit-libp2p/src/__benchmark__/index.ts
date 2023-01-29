d4b2594bf11a29fbdff6df0cdb5289aa8ff34ce6
Marcus Pousette <marcus.pousette@quantleaf.com>
2023-01-29 23:03:00 +0100

    add start stop benh

---
+++ packages/libp2p/peerbit-libp2p/src/__benchmark__/index.ts
@@ -0,0 +1,22 @@
+import B from "benchmark";
+import { createLibp2pExtended } from "../index.js";
+import { tcp } from "@libp2p/tcp";
+// Run with "node --loader ts-node/esm ./src/__benchmark__/index.ts"
+
+const suite = new B.Suite();
+suite
+	.add("start and stop", {
+		fn: async (deferred) => {
+			const node = await createLibp2pExtended({
+				libp2p: { transports: [tcp()] },
+			});
+			await node.start();
+			await node.stop();
+			deferred.resolve();
+		},
+		defer: true,
+	})
+	.on("cycle", (event: any) => {
+		console.log(String(event.target));
+	})
+	.run({ async: true });
