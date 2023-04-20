10368b2685c5c1c2f9ef3f889272c2d96e6744bf
Marcus Pousette <marcus.pousette@quantleaf.com>
2023-04-20 21:08:11 +0200

    add websocket comment

--- packages/libp2p/libp2p-test-utils/src/transports.ts
+++ packages/libp2p/libp2p-test-utils/src/transports.ts
@@ -1,11 +1,14 @@
 import { circuitRelayTransport } from "libp2p/circuit-relay";
 import { tcp } from "@libp2p/tcp";
 import { circuitRelayServer } from "libp2p/circuit-relay";
+/* import { webSockets } from "@libp2p/websockets";
+import * as filters from '@libp2p/websockets/filters' */
 export const transports = () => [
 	circuitRelayTransport({
 		discoverRelays: 1,
 	}),
 	tcp(),
+	/* webSockets({ filter: filters.all }) */
 ];
 
 export const relay = () => circuitRelayServer({});
