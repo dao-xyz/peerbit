c7965ceb91300dc918251f2ed7c40b0fc05fcb05
Marcus Pousette <marcus.pousette@quantleaf.com>
2023-04-24 09:39:11 +0200

    add reservation limit

--- packages/libp2p/peerbit-libp2p/src/transports.ts
+++ packages/libp2p/peerbit-libp2p/src/transports.ts
@@ -2,7 +2,11 @@ import { webSockets } from "@libp2p/websockets";
 import { circuitRelayTransport } from "libp2p/circuit-relay";
 import { tcp } from "@libp2p/tcp";
 import { circuitRelayServer } from "libp2p/circuit-relay";
-
-export const transports = () => [webSockets(), circuitRelayTransport(), tcp()];
-
-export const relay = () => circuitRelayServer();
+import { all } from "@libp2p/websockets/filters";
+export const transports = () => [
+	webSockets({ filter: all }),
+	circuitRelayTransport(),
+	tcp(),
+];
+export const relay = () =>
+	circuitRelayServer({ reservations: { maxReservations: 1000 } });
