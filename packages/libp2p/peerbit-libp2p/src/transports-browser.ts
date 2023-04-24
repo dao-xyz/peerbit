3f9de043b1697aa72f1d26e39d7c614277b9910c
Marcus Pousette <marcus.pousette@quantleaf.com>
2023-04-24 09:39:21 +0200

    add filter

--- packages/libp2p/peerbit-libp2p/src/transports-browser.ts
+++ packages/libp2p/peerbit-libp2p/src/transports-browser.ts
@@ -1,10 +1,13 @@
 import { webSockets } from "@libp2p/websockets";
 import { circuitRelayTransport } from "libp2p/circuit-relay";
 import { webRTC } from "@libp2p/webrtc";
+import { all } from "@libp2p/websockets/filters";
 
 export const transports = () => [
-	webSockets(),
-	circuitRelayTransport(),
+	webSockets({ filter: all }),
+	circuitRelayTransport({
+		discoverRelays: 1,
+	}),
 	webRTC({}),
 ];
 
