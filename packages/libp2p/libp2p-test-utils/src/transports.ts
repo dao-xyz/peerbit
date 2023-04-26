2b0a2035c3f00087383b43363a98bb70811b7c82
Marcus Pousette <marcus.pousette@quantleaf.com>
2023-04-26 16:38:49 +0200

    browser config

--- packages/libp2p/libp2p-test-utils/src/transports.ts
+++ packages/libp2p/libp2p-test-utils/src/transports.ts
@@ -2,17 +2,17 @@ import { circuitRelayTransport } from "libp2p/circuit-relay";
 import { tcp } from "@libp2p/tcp";
 import { circuitRelayServer } from "libp2p/circuit-relay";
 import { webRTC } from "@libp2p/webrtc";
-import { webSockets } from "@libp2p/websockets"
-import * as filters from '@libp2p/websockets/filters'
-export const transports = (browser: boolean) => browser ? [
-	circuitRelayTransport({
-		discoverRelays: 1,
-	}),
-	webRTC({}),
-	webSockets({ filter: filters.all })
-] : [
-	tcp(),
-	webSockets({ filter: filters.all })
-];
+import { webSockets } from "@libp2p/websockets";
+import * as filters from "@libp2p/websockets/filters";
+export const transports = (browser: boolean) =>
+	browser
+		? [
+				circuitRelayTransport({
+					discoverRelays: 1,
+				}),
+				webRTC({}),
+				webSockets({ filter: filters.all }),
+		  ]
+		: [tcp(), webSockets({ filter: filters.all })];
 
 export const relay = () => circuitRelayServer({});
