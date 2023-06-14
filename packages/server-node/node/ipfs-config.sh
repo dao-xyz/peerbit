6b82d5b1201469c30dfb061396f11661e6f2d79e
Marcus Pousette <marcus.pousette@quantleaf.com>
2023-06-14 17:42:31 +0200

    fix: remove redundant config file

--- packages/server-node/node/ipfs-config.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh 
-set -ex 
-ipfs bootstrap rm all 
-ipfs config Addresses.Swarm '["/ip4/0.0.0.0/tcp/4001", "/ip4/0.0.0.0/tcp/8081/ws", "/ip6/::/tcp/4001"]' --json
-ipfs config --json Pubsub.Enabled true 
-ipfs config Swarm.RelayService '{"Enabled": true}' --json
