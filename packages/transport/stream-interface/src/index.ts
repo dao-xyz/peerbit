fd82525849837da9abe0421e9387427934e10c32
Marcus Pousette <marcus.pousette@quantleaf.com>
2023-12-04 16:54:56 +0100

    fix: add event object to interface

--- packages/transport/stream-interface/src/index.ts
+++ packages/transport/stream-interface/src/index.ts
@@ -1,9 +1,17 @@
 import { PublicSignKey } from "@peerbit/crypto";
 import type { PeerId } from "@libp2p/interface/peer-id";
+import { DataMessage, Message } from "./messages.js";
 
 export interface PeerEvents {
 	"peer:reachable": CustomEvent<PublicSignKey>;
 	"peer:unreachable": CustomEvent<PublicSignKey>;
+}
+
+export interface MessageEvents {
+	message: CustomEvent<Message>;
+}
+export interface StreamEvents extends PeerEvents, MessageEvents {
+	data: CustomEvent<DataMessage>;
 }
 
 export * from "./messages.js";
