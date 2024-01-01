b3c10b87932d812f02ed242e36f2fa2b1c633322
Marcus Pousette <marcus.pousette@quantleaf.com>
2024-01-01 22:31:46 +0100

    feat: add tobase64url encode/decode

--- packages/utils/crypto/src/utils.ts
+++ packages/utils/crypto/src/utils.ts
@@ -1,15 +1,18 @@
 import sodium from "libsodium-wrappers";
-export const fromHexString = (hexString: string) =>
-	Uint8Array.from(
-		hexString.match(/.{1,2}/g)!.map((byte) => parseInt(byte, 16))
-	);
+export const fromHexString = (hexString: string) => sodium.from_hex(hexString);
 
-export const toHexString = (bytes: Uint8Array) =>
-	bytes.reduce((str, byte) => str + byte.toString(16).padStart(2, "0"), "");
+export const toHexString = (bytes: Uint8Array) => sodium.to_hex(bytes);
 
 export const toBase64 = (arr: Uint8Array) => {
 	return sodium.to_base64(arr, sodium.base64_variants.ORIGINAL);
 };
 export const fromBase64 = (base64: string) => {
 	return sodium.from_base64(base64, sodium.base64_variants.ORIGINAL);
+};
+
+export const toBase64URL = (arr: Uint8Array) => {
+	return sodium.to_base64(arr, sodium.base64_variants.URLSAFE);
+};
+export const fromBase64URL = (base64: string) => {
+	return sodium.from_base64(base64, sodium.base64_variants.URLSAFE);
 };
