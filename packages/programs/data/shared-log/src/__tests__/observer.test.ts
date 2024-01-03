a387df2c553dcd4bb85c7b19d3a4decb7f318ad6
Marcus Pousette <marcus.pousette@quantleaf.com>
2024-01-03 13:35:13 +0100

    fix: add can wait for replicator as observer test

--- packages/programs/data/shared-log/src/__tests__/observer.test.ts
+++ packages/programs/data/shared-log/src/__tests__/observer.test.ts
@@ -3,26 +3,36 @@ import { EventStore } from "./utils/stores";
 import { Observer, Replicator } from "../role";
 import { waitForResolved } from "@peerbit/time";
 import { deserialize, serialize } from "@dao-xyz/borsh";
+const replicatorBelowIndex = [0, 1];
 
 describe("observer", () => {
 	let session: TestSession;
 
-	beforeAll(async () => {
-		session = await TestSession.connected(3);
-	});
+	beforeAll(async () => {});
 
 	afterAll(async () => {
 		await session.stop();
 	});
 
 	it("observers will not receive heads", async () => {
+		session = await TestSession.disconnected(3);
+		session.connect([
+			[session.peers[0], session.peers[1]],
+			[session.peers[1], session.peers[2]]
+		]);
+
 		let stores: EventStore<string>[] = [];
 		const s = new EventStore<string>();
 		const createStore = () => deserialize(serialize(s), EventStore);
+		let replicatorEndIndex = 1;
+
 		for (const [i, peer] of session.peers.entries()) {
 			const store = await peer.open(createStore(), {
 				args: {
-					role: i <= 1 ? { type: "replicator", factor: 1 } : "observer"
+					role:
+						i <= replicatorEndIndex
+							? { type: "replicator", factor: 1 }
+							: "observer"
 				}
 			});
 			stores.push(store);
@@ -34,6 +44,10 @@ describe("observer", () => {
 					continue;
 				}
 				await store.waitFor(peer.peerId, { timeout: 10 * 1000 });
+
+				if (j <= replicatorEndIndex) {
+					await store.log.waitForReplicator(peer.identity.publicKey);
+				}
 			}
 		}
 
@@ -53,5 +67,31 @@ describe("observer", () => {
 				}
 			}
 		}
+	});
+
+	it("can wait for replicator", async () => {
+		session = await TestSession.disconnected(3);
+		await session.connect([
+			[session.peers[0], session.peers[1]],
+			[session.peers[1], session.peers[2]]
+		]);
+
+		const s = new EventStore<string>();
+		const createStore = () => deserialize(serialize(s), EventStore);
+		const replicator = await session.peers[0].open(createStore(), {
+			args: {
+				role: { type: "replicator", factor: 1 }
+			}
+		});
+
+		const observer = await session.peers[2].open(createStore(), {
+			args: {
+				role: "observer"
+			}
+		});
+
+		await observer.log.waitForReplicator(replicator.node.identity.publicKey);
+		expect(observer.log.getReplicatorsSorted()?.length).toEqual(1);
+		expect(observer.log.role).toBeInstanceOf(Observer);
 	});
 });
