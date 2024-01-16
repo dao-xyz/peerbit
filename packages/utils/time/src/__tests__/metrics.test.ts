516f9e00434037e39dcf4b53765b1350eda8b04e
Marcus Pousette <marcus.pousette@quantleaf.com>
2024-01-16 16:05:24 +0100

    fix: test using interval

--- packages/utils/time/src/__tests__/metrics.test.ts
+++ packages/utils/time/src/__tests__/metrics.test.ts
@@ -1,14 +1,20 @@
-import { delay } from "..";
+import { delay, waitForResolved } from "..";
 import { MovingAverageTracker } from "../metrics.js";
 
 describe("metrics", () => {
 	it("moving averiage", async () => {
 		const ma = new MovingAverageTracker(1);
-		for (let i = 0; i < 200; i++) {
-			ma.add(50); // 50 per 50 ms => 1000 per second
-			await delay(50);
-		}
-		expect(ma.value).toBeGreaterThan(900);
-		expect(ma.value).toBeLessThan(1050);
+		let done = false;
+		const interval = setInterval(() => {
+			ma.add(100); // 100 per 100 ms => 1000 per second
+			try {
+				expect(ma.value).toBeGreaterThan(900);
+				expect(ma.value).toBeLessThan(1050);
+				clearInterval(interval);
+				done = true;
+			} catch (error) {}
+		}, 100);
+
+		await waitForResolved(() => expect(done).toBeTrue());
 	});
 });
