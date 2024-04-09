da2d47b401e184cb0a6552032867f7ef83f174b1
Marcus Pousette <marcus.pousette@quantleaf.com>
2024-04-09 15:57:36 +0200

    fix: remove should

--- chai-global.js
+++ chai-global.js
@@ -1,7 +1,6 @@
-import { should, use, expect } from "chai";
+import { use } from "chai";
 import chaiAsPromised from 'chai-as-promised';
 use(chaiAsPromised);
-should()
 
 /* 
 process.env.TS_NODE_PROJECT = 'tsconfig.test.json';
