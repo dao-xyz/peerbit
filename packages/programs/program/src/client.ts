9813c9872029f7937449b687fa3477afd979e001
Marcus Pousette <marcus.pousette@quantleaf.com>
2024-04-10 09:21:29 +0200

    fix: import only type

--- packages/programs/program/src/client.ts
+++ packages/programs/program/src/client.ts
@@ -1,7 +1,7 @@
 import type { PeerId as Libp2pPeerId } from "@libp2p/interface";
 import { type Blocks } from "@peerbit/blocks-interface";
 import { type PubSub } from "@peerbit/pubsub-interface";
-import { Ed25519PublicKey, type Identity } from "@peerbit/crypto";
+import { type Ed25519PublicKey, type Identity } from "@peerbit/crypto";
 import { type Keychain } from "@peerbit/keychain";
 import type { AnyStore } from "@peerbit/any-store-interface";
 import type { Multiaddr } from "@multiformats/multiaddr";
