a84c2fcbc5405a3978b930ea50dbfca838162d23
Marcus Pousette <marcus.pousette@quantleaf.com>
2024-07-28 10:56:40 +0200

    fix: add max search depth for node_modules

--- packages/clients/vite/src/index.ts
+++ packages/clients/vite/src/index.ts
@@ -35,7 +35,7 @@ function copyToPublicPlugin(
 					const sourcePath = path.resolve(src);
 					const destinationPath = path.resolve(process.cwd(), "public", dest);
 
-					copyAssets(sourcePath, destinationPath);
+					copyAssets(sourcePath, destinationPath, "/");
 				});
 			}
 		},
@@ -44,6 +44,7 @@ function copyToPublicPlugin(
 
 const findLibraryInNodeModules = (library: string) => {
 	// scan upwards until we find the node_modules folder
+	let maxSearchDepth = 10;
 	let currentDir = process.cwd();
 	let nodeModulesDir = path.join(currentDir, "node_modules");
 
@@ -55,6 +56,11 @@ const findLibraryInNodeModules = (library: string) => {
 		// then stop
 		if (fs.existsSync(path.join(currentDir, ".git"))) {
 			break;
+		}
+
+		maxSearchDepth--;
+		if (maxSearchDepth <= 0) {
+			throw new Error(`Could not find ${library} node_modules folder`);
 		}
 	}
 	const libraryPath = path.join(nodeModulesDir, library);
@@ -97,27 +103,23 @@ export default (
 	];
 };
 
-function copyAssets(srcPath: string, destPath: string) {
+function copyAssets(srcPath: string, destPath: string, base: string) {
 	if (!fs.existsSync(srcPath)) {
 		throw new Error(`File ${srcPath} does not exist`);
 	}
 
 	if (fs.statSync(srcPath).isDirectory()) {
-		// Ensure the directory exists in the public folder
 		fs.mkdirSync(destPath, { recursive: true });
 
-		// Copy each file/directory inside the current directory
 		fs.readdirSync(srcPath).forEach((file) => {
 			const srcFilePath = path.join(srcPath, file);
 			const destFilePath = path.join(destPath, file);
 
-			copyAssets(srcFilePath, destFilePath); // Recursion for directories
+			copyAssets(srcFilePath, destFilePath, base);
 		});
 	} else {
-		// Ensure the destination directory exists
 		fs.mkdirSync(path.dirname(destPath), { recursive: true });
 
-		// Copy the file
 		fs.copyFileSync(srcPath, destPath);
 	}
 }
