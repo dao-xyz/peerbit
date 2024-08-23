5b75d85db565faf76b53a2b81695387f7e622bbb
Marcus Pousette <marcus.pousette@quantleaf.com>
2024-08-23 19:53:37 +0200

    chore: rm nested function

--- packages/programs/rpc/src/utils.ts
+++ packages/programs/rpc/src/utils.ts
@@ -19,7 +19,7 @@ export class MissingResponsesError extends Error {
 export type RPCRequestAllOptions<R> = RPCRequestResponseOptions<R> &
 	EncryptionOptions & { mode?: Constructor<DeliveryMode> } & PriorityOptions;
 
-export const queryAll = <Q, R>(
+export const queryAll = async <Q, R>(
 	rpc: RPC<Q, R>,
 	groups: string[][],
 	request: Q,
@@ -34,56 +34,55 @@ export const queryAll = <Q, R>(
 	const sendModeType = options?.mode || SilentDelivery;
 	let rng = Math.round(Math.random() * groups.length);
 	const startRng = rng;
-	const fn = async () => {
-		let missingReponses = false;
-		while (groups.length > 0) {
-			const peersToQuery: string[] = new Array(groups.length);
-			let counter = 0;
-			const peerToGroupIndex = new Map<string, number>();
-			for (let i = 0; i < groups.length; i++) {
-				const group = groups[i];
-				peersToQuery[counter] = group[rng % group.length];
-				peerToGroupIndex.set(peersToQuery[counter], i);
-				counter++;
-			}
-			if (peersToQuery.length > 0) {
-				const results = await rpc.request(request, {
-					...options,
-					// eslint-disable-next-line new-cap
-					mode: new sendModeType({ to: peersToQuery, redundancy: 1 }), // TODO configuration redundancy?
-				});
 
-				for (const result of results) {
-					if (!result.from) {
-						throw new Error("Unexpected, missing from");
-					}
-					peerToGroupIndex.delete(result.from.hashcode());
+	let missingReponses: string[][] | undefined = undefined;
+	while (groups.length > 0) {
+		const peersToQuery: string[] = new Array(groups.length);
+		let counter = 0;
+		const peerToGroupIndex = new Map<string, number>();
+		for (let i = 0; i < groups.length; i++) {
+			const group = groups[i];
+			peersToQuery[counter] = group[rng % group.length];
+			peerToGroupIndex.set(peersToQuery[counter], i);
+			counter++;
+		}
+		if (peersToQuery.length > 0) {
+			const results = await rpc.request(request, {
+				...options,
+				// eslint-disable-next-line new-cap
+				mode: new sendModeType({ to: peersToQuery, redundancy: 1 }), // TODO configuration redundancy?
+			});
+
+			for (const result of results) {
+				if (!result.from) {
+					throw new Error("Unexpected, missing from");
 				}
-
-				await responseHandler(results);
-
-				const indicesLeft = new Set([...peerToGroupIndex.values()]);
-
-				rng += 1;
-				groups = groups.filter((v, ix) => {
-					if (indicesLeft.has(ix)) {
-						const peerIndex = rng % v.length;
-						if (rng === startRng || peerIndex === startRng % v.length) {
-							// TODO Last condition needed?
-							missingReponses = true;
-							return false;
-						}
-						return true;
-					}
-					return false;
-				});
+				peerToGroupIndex.delete(result.from.hashcode());
 			}
+
+			await responseHandler(results);
+
+			const indicesLeft = new Set([...peerToGroupIndex.values()]);
+
+			rng += 1;
+			groups = groups.filter((v, ix) => {
+				if (indicesLeft.has(ix)) {
+					const peerIndex = rng % v.length;
+					if (rng === startRng || peerIndex === startRng % v.length) {
+						// TODO Last condition needed?
+						(missingReponses || (missingReponses = [])).push(v);
+						return false;
+					}
+					return true;
+				}
+				return false;
+			});
 		}
-		if (missingReponses) {
-			throw new MissingResponsesError(
-				"Did not receive responses from all shards",
-			);
-		}
-	};
-	return fn();
+	}
+	if (missingReponses) {
+		throw new MissingResponsesError(
+			"Did not receive responses from all shards: " +
+				JSON.stringify(missingReponses),
+		);
+	}
 };
