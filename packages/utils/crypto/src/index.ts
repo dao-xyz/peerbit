888bb513ebb1cbc51946597f970cfc7f56b7d6d0
Marcus Pousette <marcus.pousette@quantleaf.com>
2024-10-11 22:29:17 +0200

    fix: update libp2p

--- packages/utils/crypto/src/index.ts
+++ packages/utils/crypto/src/index.ts
@@ -6,12 +6,13 @@ export * from "./signature.js";
 export * from "./sepc256k1.js";
 export * from "./x25519.js";
 export * from "./encryption.js";
-export * from "./peerid.js";
+export * from "./from.js";
 export * from "./utils.js";
 export * from "./hash.js";
 export * from "./random.js";
 export * from "./prehash.js";
 export * from "./signer.js";
+export * from "./from.js";
 
 const ready = libsodium.ready;
 export { ready };
