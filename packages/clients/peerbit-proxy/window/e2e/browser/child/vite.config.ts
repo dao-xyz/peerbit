2d632f172a5e3dccedf91338a5ed050b3088eef9
Marcus Pousette <marcus.pousette@quantleaf.com>
2024-12-30 21:19:17 +0100

    chore: rm unecessary copy

--- packages/clients/peerbit-proxy/window/e2e/browser/child/vite.config.ts
+++ packages/clients/peerbit-proxy/window/e2e/browser/child/vite.config.ts
@@ -1,22 +1,10 @@
 import peerbit from "@peerbit/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import includeAssetsPlugin from "./../plugin.js";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		react(),
-		includeAssetsPlugin({
-			assets: [
-				{
-					src: "../../../../../../../node_modules/@peerbit/indexer-sqlite3/dist/peerbit",
-					dest: "peerbit/",
-				},
-			],
-		}),
-		peerbit(),
-	],
+	plugins: [react(), peerbit()],
 	optimizeDeps: {
 		esbuildOptions: {
 			target: "esnext",
