617e97ca45b5b896f82859170cc7025d2e742f95
Marcus Pousette <marcus.pousette@quantleaf.com>
2025-02-07 16:28:27 +0100

    test: add canMerge tests

--- packages/programs/data/document/document/test/domain.spec.ts
+++ packages/programs/data/document/document/test/domain.spec.ts
@@ -1,7 +1,9 @@
 import { field, variant } from "@dao-xyz/borsh";
+import { Ed25519Keypair } from "@peerbit/crypto";
 import { SearchRequest } from "@peerbit/document-interface";
 import { Sort } from "@peerbit/indexer-interface";
 import { Program } from "@peerbit/program";
+import { ReplicationRangeIndexableU64 } from "@peerbit/shared-log";
 import { TestSession } from "@peerbit/test-utils";
 import { waitForResolved } from "@peerbit/time";
 import { expect } from "chai";
@@ -56,148 +58,265 @@ export class StoreWithCustomDomain extends Program {
 }
 
 describe("domain", () => {
-	let session: TestSession;
-	let store: StoreWithCustomDomain, store2: StoreWithCustomDomain;
+	describe("search replicate", () => {
+		let session: TestSession;
+		let store: StoreWithCustomDomain, store2: StoreWithCustomDomain;
 
-	before(async () => {
-		session = await TestSession.connected(2);
-	});
-
-	after(async () => {
-		await session.stop();
-	});
-
-	beforeEach(async () => {
-		store = await session.peers[0].open(new StoreWithCustomDomain(), {
-			args: {
-				replicate: {
-					normalized: false,
-					factor: 1,
-					offset: 1,
-					strict: true,
-				},
-			},
-		});
-		store2 = await session.peers[1].open(store.clone(), {
-			args: {
-				replicate: {
-					normalized: false,
-					offset: 2,
-					factor: 2,
-					strict: true,
-				},
-			},
+		before(async () => {
+			session = await TestSession.connected(2);
 		});
 
-		await store.docs.put(new Document({ id: "1", property: 1 }));
-		await store2.docs.put(new Document({ id: "2", property: 2 }));
-		await store2.docs.put(new Document({ id: "3", property: 3 }));
-
-		await waitForResolved(async () => {
-			expect(await store.docs.index.getSize()).to.equal(1);
-			expect(await store2.docs.index.getSize()).to.equal(2);
+		after(async () => {
+			await session.stop();
 		});
 
-		await store.docs.log.waitForReplicator(store2.docs.node.identity.publicKey);
-		await store2.docs.log.waitForReplicator(store.docs.node.identity.publicKey);
-	});
-
-	afterEach(async () => {
-		await store.close();
-		await store2.close();
-	});
-
-	it("custom domain", async () => {
-		// test querying with the same domain but different peers and assert results are correct
-		const resultsWithRemoteRightDomain = await store.docs.index.search(
-			new SearchRequest(),
-			{
-				remote: {
-					domain: {
-						from: 2n,
-						to: 3n,
+		beforeEach(async () => {
+			store = await session.peers[0].open(new StoreWithCustomDomain(), {
+				args: {
+					replicate: {
+						normalized: false,
+						factor: 1,
+						offset: 1,
+						strict: true,
 					},
-					eager: true,
 				},
-			},
-		);
-
-		expect(resultsWithRemoteRightDomain).to.have.length(3);
-
-		const resultsWhenRemoteDoesNotHaveRightDomain =
-			await store.docs.index.search(new SearchRequest(), {
-				remote: {
-					domain: {
-						from: 4n,
-						to: 5n,
+			});
+			store2 = await session.peers[1].open(store.clone(), {
+				args: {
+					replicate: {
+						normalized: false,
+						offset: 2,
+						factor: 2,
+						strict: true,
 					},
 				},
 			});
 
-		expect(resultsWhenRemoteDoesNotHaveRightDomain).to.have.length(1); // only the loal result
-		expect(resultsWhenRemoteDoesNotHaveRightDomain[0].id).to.equal("1");
+			await store.docs.put(new Document({ id: "1", property: 1 }));
+			await store2.docs.put(new Document({ id: "2", property: 2 }));
+			await store2.docs.put(new Document({ id: "3", property: 3 }));
+
+			await waitForResolved(async () => {
+				expect(await store.docs.index.getSize()).to.equal(1);
+				expect(await store2.docs.index.getSize()).to.equal(2);
+			});
+
+			await store.docs.log.waitForReplicator(
+				store2.docs.node.identity.publicKey,
+			);
+			await store2.docs.log.waitForReplicator(
+				store.docs.node.identity.publicKey,
+			);
+		});
+
+		afterEach(async () => {
+			await store.close();
+			await store2.close();
+		});
+
+		it("custom domain", async () => {
+			// test querying with the same domain but different peers and assert results are correct
+			const resultsWithRemoteRightDomain = await store.docs.index.search(
+				new SearchRequest(),
+				{
+					remote: {
+						domain: {
+							from: 2n,
+							to: 3n,
+						},
+						eager: true,
+					},
+				},
+			);
+
+			expect(resultsWithRemoteRightDomain).to.have.length(3);
+
+			const resultsWhenRemoteDoesNotHaveRightDomain =
+				await store.docs.index.search(new SearchRequest(), {
+					remote: {
+						domain: {
+							from: 4n,
+							to: 5n,
+						},
+					},
+				});
+
+			expect(resultsWhenRemoteDoesNotHaveRightDomain).to.have.length(1); // only the loal result
+			expect(resultsWhenRemoteDoesNotHaveRightDomain[0].id).to.equal("1");
+		});
+
+		it("will join with multiple segments when not sorting by mergeable property", async () => {
+			expect(
+				(await store.docs.log.getMyReplicationSegments()).map((x) =>
+					[x.start1, x.end1].map(Number),
+				),
+			).to.deep.equal([[1, 2]]);
+			const resultsWithRemoteRightDomain = await store.docs.index.search(
+				new SearchRequest(),
+				{
+					remote: {
+						domain: {
+							from: 0n,
+							to: 5n,
+						},
+						eager: true,
+						replicate: true,
+					},
+				},
+			);
+
+			expect(resultsWithRemoteRightDomain).to.have.length(3);
+			expect(
+				(await store.docs.log.getMyReplicationSegments()).map((x) =>
+					[x.start1, x.end1].map(Number),
+				),
+			).to.deep.equal([
+				[1, 2],
+				[2, 3],
+				[3, 4],
+			]);
+		});
+
+		it("will join by separate segments when not sorting by non mergeable property", async () => {
+			expect(
+				(await store.docs.log.getMyReplicationSegments()).map((x) =>
+					[x.start1, x.end1].map(Number),
+				),
+			).to.deep.equal([[1, 2]]);
+			const resultsWithRemoteRightDomain = await store.docs.index.search(
+				new SearchRequest({ sort: [new Sort({ key: ["property"] })] }),
+				{
+					remote: {
+						domain: {
+							from: 0n,
+							to: 5n,
+						},
+						eager: true,
+						replicate: true,
+					},
+				},
+			);
+
+			expect(resultsWithRemoteRightDomain).to.have.length(3);
+			expect(
+				(await store.docs.log.getMyReplicationSegments()).map((x) =>
+					[x.start1, x.end1].map(Number),
+				),
+			).to.deep.equal([
+				[1, 2],
+				[2, 4],
+			]);
+		});
 	});
 
-	it("will join with multiple segments when not sorting by mergeable property", async () => {
-		expect(
-			(await store.docs.log.getMyReplicationSegments()).map((x) =>
-				[x.start1, x.end1].map(Number),
-			),
-		).to.deep.equal([[1, 2]]);
-		const resultsWithRemoteRightDomain = await store.docs.index.search(
-			new SearchRequest(),
-			{
-				remote: {
-					domain: {
-						from: 0n,
-						to: 5n,
-					},
-					eager: true,
-					replicate: true,
-				},
-			},
-		);
+	describe("canMerge", () => {
+		it("canMerge into same range", async () => {
+			const kp = await Ed25519Keypair.create();
+			const domain = createDocumentDomainFromProperty({
+				property: "property",
+				resolution: "u64",
+				mergeSegmentMaxDelta: 10,
+			})(undefined as any);
 
-		expect(resultsWithRemoteRightDomain).to.have.length(3);
-		expect(
-			(await store.docs.log.getMyReplicationSegments()).map((x) =>
-				[x.start1, x.end1].map(Number),
-			),
-		).to.deep.equal([
-			[1, 2],
-			[2, 3],
-			[3, 4],
-		]);
-	});
+			const from = new ReplicationRangeIndexableU64({
+				offset: 0n,
+				width: 100n,
+				publicKey: kp.publicKey,
+			});
 
-	it("will join by separate segments when not sorting by non mergeable property", async () => {
-		expect(
-			(await store.docs.log.getMyReplicationSegments()).map((x) =>
-				[x.start1, x.end1].map(Number),
-			),
-		).to.deep.equal([[1, 2]]);
-		const resultsWithRemoteRightDomain = await store.docs.index.search(
-			new SearchRequest({ sort: [new Sort({ key: ["property"] })] }),
-			{
-				remote: {
-					domain: {
-						from: 0n,
-						to: 5n,
-					},
-					eager: true,
-					replicate: true,
-				},
-			},
-		);
+			expect(domain.canMerge!(from, from)).to.be.true;
+		});
 
-		expect(resultsWithRemoteRightDomain).to.have.length(3);
-		expect(
-			(await store.docs.log.getMyReplicationSegments()).map((x) =>
-				[x.start1, x.end1].map(Number),
-			),
-		).to.deep.equal([
-			[1, 2],
-			[2, 4],
-		]);
+		it("canMerge overlapping ranges", async () => {
+			const kp = await Ed25519Keypair.create();
+			const domain = createDocumentDomainFromProperty({
+				property: "property",
+				resolution: "u64",
+				mergeSegmentMaxDelta: 0,
+			})(undefined as any);
+
+			const from = new ReplicationRangeIndexableU64({
+				offset: 0n,
+				width: 60n,
+				publicKey: kp.publicKey,
+			});
+
+			const to = new ReplicationRangeIndexableU64({
+				offset: 50n,
+				width: 60n,
+				publicKey: kp.publicKey,
+			});
+
+			expect(domain.canMerge!(from, to)).to.be.true;
+		});
+
+		it("canMerge not merge too large gap", async () => {
+			const kp = await Ed25519Keypair.create();
+			const domain = createDocumentDomainFromProperty({
+				property: "property",
+				resolution: "u64",
+				mergeSegmentMaxDelta: 9,
+			})(undefined as any);
+
+			const from = new ReplicationRangeIndexableU64({
+				offset: 0n,
+				width: 50n,
+				publicKey: kp.publicKey,
+			});
+
+			const to = new ReplicationRangeIndexableU64({
+				offset: 60n,
+				width: 50n,
+				publicKey: kp.publicKey,
+			});
+
+			expect(domain.canMerge!(from, to)).to.be.false;
+		});
+
+		it("canMerge not merge small enough gap", async () => {
+			const kp = await Ed25519Keypair.create();
+			const domain = createDocumentDomainFromProperty({
+				property: "property",
+				resolution: "u64",
+				mergeSegmentMaxDelta: 10,
+			})(undefined as any);
+
+			const from = new ReplicationRangeIndexableU64({
+				offset: 0n,
+				width: 50n,
+				publicKey: kp.publicKey,
+			});
+
+			const to = new ReplicationRangeIndexableU64({
+				offset: 60n,
+				width: 50n,
+				publicKey: kp.publicKey,
+			});
+
+			expect(domain.canMerge!(from, to)).to.be.true;
+		});
+
+		it("canMerge almost exactly overlapping", async () => {
+			const kp = await Ed25519Keypair.create();
+			const domain = createDocumentDomainFromProperty({
+				property: "property",
+				resolution: "u64",
+				mergeSegmentMaxDelta: 10,
+			})(undefined as any);
+
+			const from = new ReplicationRangeIndexableU64({
+				offset: 0n,
+				width: 90n,
+				publicKey: kp.publicKey,
+			});
+
+			const to = new ReplicationRangeIndexableU64({
+				offset: 10n,
+				width: 90n,
+				publicKey: kp.publicKey,
+			});
+
+			expect(domain.canMerge!(from, to)).to.be.true;
+		});
 	});
 });
