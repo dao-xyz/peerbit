6684716c109afcc76f5a3736f99545e7291dc20f
Marcus Pousette <marcus.pousette@live.com>
2025-11-24 20:52:03 +0800

    fix: add public folder

--- packages/programs/data/document/react/e2e/party/browser-node/vite.config.ts
+++ packages/programs/data/document/react/e2e/party/browser-node/vite.config.ts
@@ -1,9 +1,15 @@
 import peerbit from "@peerbit/vite";
 import react from "@vitejs/plugin-react";
+import fs from "node:fs";
 import path from "node:path";
 import { defineConfig } from "vite";
 
 const ROOT = path.resolve(__dirname, "..");
+const PUBLIC_DIR = path.resolve(__dirname, "public");
+
+if (!fs.existsSync(PUBLIC_DIR)) {
+	fs.mkdirSync(PUBLIC_DIR, { recursive: true });
+}
 
 export default defineConfig({
 	plugins: [react(), peerbit()],
