3797bbc3782717a85bd26790924a49730f1d1076
Marcus Pousette <marcus.pousette@live.com>
2025-11-26 17:08:57 +0800

    fix: module bundling

--- packages/utils/any-store/opfs/src/create.ts
+++ packages/utils/any-store/opfs/src/create.ts
@@ -1,7 +1,7 @@
 const createWorker = (directory: string) => {
 	const worker = new Worker(
 		new URL("/peerbit/opfs/worker.js#" + directory, import.meta.url),
-		{ type: "classic" },
+		{ type: "module" },
 	);
 	return worker;
 };
