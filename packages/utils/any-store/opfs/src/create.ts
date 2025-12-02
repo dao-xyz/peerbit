20fc6e6571676defbd0a7a1c18eb14ce423a7ff2
Marcus Pousette <marcus.pousette@live.com>
2025-12-02 14:44:32 +0800

    fix: bundle as classic worker

--- packages/utils/any-store/opfs/src/create.ts
+++ packages/utils/any-store/opfs/src/create.ts
@@ -1,7 +1,7 @@
 const createWorker = (directory: string) => {
 	const worker = new Worker(
-		new URL("/peerbit/opfs/worker.js#" + directory, import.meta.url),
-		{ type: "module" },
+		new URL("/peerbit/opfs/opfs.worker.min.js#" + directory, import.meta.url),
+		{ type: "classic" },
 	);
 	return worker;
 };
