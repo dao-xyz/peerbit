07b1221a693db6c2ff3d28c96919c9b8d13bad51
Marcus Pousette <marcus.pousette@live.com>
2026-01-26 10:18:11 +0100

    test(document): reduce waitForReplicator flakes

--- packages/programs/data/document/document/test/domain.spec.ts
+++ packages/programs/data/document/document/test/domain.spec.ts
@@ -102,13 +102,16 @@ describe("domain", () => {
 				expect(await store2.docs.index.getSize()).to.equal(2);
 			});
 
-			await store.docs.log.waitForReplicator(
-				store2.docs.node.identity.publicKey,
-			);
-			await store2.docs.log.waitForReplicator(
-				store.docs.node.identity.publicKey,
-			);
-		});
+				const waitForReplicatorTimeoutMs = 45_000;
+				await Promise.all([
+					store.docs.log.waitForReplicator(store2.docs.node.identity.publicKey, {
+						timeout: waitForReplicatorTimeoutMs,
+					}),
+					store2.docs.log.waitForReplicator(store.docs.node.identity.publicKey, {
+						timeout: waitForReplicatorTimeoutMs,
+					}),
+				]);
+			});
 
 		afterEach(async () => {
 			await store.close();
