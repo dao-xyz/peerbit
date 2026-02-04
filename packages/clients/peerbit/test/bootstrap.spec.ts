3b250818e6c97e859afee5f7e6c28caae0b58e12
Marcus Pousette <marcus.pousette@live.com>
2026-02-04 18:00:35 +0100

    ci: fix site build and bootstrap test

--- packages/clients/peerbit/test/bootstrap.spec.ts
+++ packages/clients/peerbit/test/bootstrap.spec.ts
@@ -3,16 +3,27 @@ import { Peerbit } from "../src/peer.js";
 
 describe("bootstrap", () => {
 	let peer: Peerbit;
+	let bootstrapPeer: Peerbit;
 
 	beforeEach(async () => {
+		bootstrapPeer = await Peerbit.create();
 		peer = await Peerbit.create();
 	});
 
 	afterEach(async () => {
 		await peer?.stop();
+		await bootstrapPeer?.stop();
 	});
 
-	it("remote", async () => {
+	it("local", async () => {
+		await peer.bootstrap(bootstrapPeer.getMultiaddrs());
+		expect(peer.libp2p.services.pubsub.peers.size).greaterThan(0);
+	});
+
+	it("remote", async function () {
+		if (process.env.PEERBIT_RUN_REMOTE_BOOTSTRAP_TEST !== "1") {
+			this.skip();
+		}
 		await peer.bootstrap();
 		expect(peer.libp2p.services.pubsub.peers.size).greaterThan(0);
 	});
